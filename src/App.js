022e70d0f12b32ab1ca92896f5528a25bb7c7e1e
juhyunkil <79089253+juhyunkil@users.noreply.github.com>
2021-04-26 17:54:10 +0900

    app.js 수정 kjh

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import { BrowserRouter, Route} from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import AdminPage from "./admin/side/AdminPage";
-import UsersPage from "./users/side/UsersPage";
-import LoginPage from './common/login/LoginPage';
+import AdminPage from "./admin/side/AdminPage";//관리자 페이지
+import UsersPage from "./users/side/UsersPage";//영업자 페이지
+import LoginPage from './common/login/LoginPage';//로그인페이지(구현전)
 
 
-class App extends Component {
+class App extends Component {;
+  /*로그인 하려고 하는곳인데 아직...
+    render() {
+      return (
+          <BrowserRouter>
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/admin" component={AdminPage} />
+            <Route path="/users" component={UsersPage} />
+          </BrowserRouter>
+      );
+  }
+  */
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/admin" component={AdminPage} />
-          <Route path="/users" component={UsersPage} />
-        </BrowserRouter>
-        
+        {/*관리자 페이지 컴포넌트*/}
+        <AdminPage/>
+        {/*이건 영업자 페이지 컴포넌트<UsersPage/>*/}
       </div>
     );
   }
