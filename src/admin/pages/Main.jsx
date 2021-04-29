0083b4ea471f1e78bb617f05a00271bb5789c21e
Hong SeungWon <s201703810@hufs-gsuite.kr>
2021-04-30 03:39:43 +0900

    hsw

--- src/admin/pages/Main.jsx
+++ src/admin/pages/Main.jsx
@@ -1,17 +1,55 @@
-import React, { Component } from 'react';
-import TableEx from '../contents/TableEx';
-//import CircleChart from '../content/CircleChart';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Main_table from "../contents/Main_table";
+import Main_openselect from "../contents/Main_openselect";
+import Button from '@material-ui/core/Button';
+import { Input } from '@material-ui/core';
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                {/*<CircleChart/>*/}
-                관리자 메인
-                <TableEx/>
-            </div>
-        );
-    }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Main() {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>원형그래프</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+              <Main_openselect/>
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+          2021.03.29-2021.04.04 목표
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+              <Main_table/>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Main;
--- src/admin/pages/Workers2.jsx
+++ src/admin/pages/Workers2.jsx
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Worker2_table from "../contents/Worker2_table";
+import Button from '@material-ui/core/Button';
 
-class Workers2 extends Component {
-    render() {
-        return (
-            <div>
-                직원관리
-            </div>
-        );
-    }
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Workers2() {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+              <Grid item xs>
+              <Paper className={classes.paper}>
+              사원명  <input></input> <Button variant="contained" color="primary">검색</Button>
+              </Paper>
+              </Grid>
+           
+        </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+          <Button variant="contained" color="primary">직원추가</Button>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <Button variant="contained" color="primary">직원삭제</Button>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+              <Worker2_table/>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Workers2;
